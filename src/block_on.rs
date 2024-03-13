26ec450948bbc27da89fcbf0b567470a37f41082
sothouth <55689222+sothouth@users.noreply.github.com>
2024-03-14 01:12:56 +0800

    implement a easy block_on

---
+++ src/block_on.rs
@@ -0,0 +1,32 @@
+use waker_fn::waker_fn;
+// use futures_lite::pin;
+use crossbeam::sync::Parker;
+use std::future::Future;
+use std::pin::pin;
+use std::task::{Context, Poll};
+use std::thread;
+
+pub fn block_on<F: Future>(future: F) -> F::Output {
+    let parker = Parker::new();
+    let unparker = parker.unparker().clone();
+    let waker = waker_fn(move || unparker.unpark());
+    let mut cx = Context::from_waker(&waker);
+
+    let mut pinned_fut = pin!(future);
+    loop {
+        match pinned_fut.as_mut().poll(&mut cx) {
+            Poll::Pending => parker.park(),
+            Poll::Ready(res) => return res,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn try_block_on() {
+        use crate::block_on::block_on;
+        use crate::spawn_blocking::spawn_blocking;
+        assert_eq!(block_on(spawn_blocking(|| { 1 })), 1);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@
 mod temp;
 
 pub mod spawn_blocking;
+pub mod block_on;
