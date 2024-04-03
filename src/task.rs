f856a18203d1bd8a4ba5a900896f5b990bbf7744
sothouth <55689222+sothouth@users.noreply.github.com>
2024-04-03 23:40:16 +0800

    try impl task

--- src/task.rs
+++ src/task.rs
@@ -2,3 +2,15 @@
 pub mod thread_waker;
 pub mod empty_waker;
 pub mod parker_and_waker;
+pub mod option_waker;
+
+
+use std::future::Future;
+use std::sync::{Arc,atomic::{AtomicUsize,Ordering::*}};
+use std::task::{Context,Waker,Poll};
+
+pub struct Task<F,W>{
+    state:AtomicUsize,
+    future:F,
+    waker:W,
+}
