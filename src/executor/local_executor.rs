640cc9f6c47d16918bbf8cabdd0c6056dc6cfb67
sothouth <55689222+sothouth@users.noreply.github.com>
2024-04-05 02:38:58 +0800

    try to architect async

--- src/executor/local_executor.rs
+++ src/executor/local_executor.rs
@@ -1,1 +1,31 @@
+use std::future::{poll_fn, Future};
+use std::pin::{pin, Pin};
+/// tring
+use std::sync::{Arc, Mutex, RwLock};
+use std::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
+use std::thread;
+use std::time::{Duration, Instant};
 
+pub struct Builder {}
+
+pub struct Excutor {}
+
+pub struct LocalExecutor {}
+
+pub struct Task {
+    future: Pin<Box<dyn Future<Output = ()> + Send>>,
+}
+
+#[test]
+fn t() {
+    use async_executor::Executor;
+    use futures_lite::future;
+
+    let ex = Executor::new();
+
+    let task = ex.spawn(async { 1 + 2 });
+    let t2 = ex.spawn(async { "hello" });
+    let res = future::block_on(ex.run(async { task.await * 2 }));
+
+    assert_eq!(res, 6);
+}
