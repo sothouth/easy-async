ea4422608b26efb591d6f47fef38be1a5fb156a7
sothouth <55689222+sothouth@users.noreply.github.com>
2024-04-09 23:13:30 +0800

    split no_output_executor into no_output_executor, task, task::waker

--- src/executor/no_output_executor.rs
+++ src/executor/no_output_executor.rs
@@ -1,18 +1,14 @@
 //! No output executor
 //!
 //! In \tests\executor.rs: no_output_many_async 10 times faster than smol_many_async
-use std::cell::UnsafeCell;
 use std::future::Future;
-use std::mem::ManuallyDrop;
-use std::pin::Pin;
 use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering::*};
 use std::sync::{Arc, Mutex, OnceLock};
-use std::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
-use std::{fmt, thread};
+use std::thread;
 
 use concurrent_queue::ConcurrentQueue;
 
-use crate::waker::AtomicWaker;
+use crate::task::task::{task_and_handle, TaskHandle};
 
 static GLOBAL: OnceLock<Executor> = OnceLock::new();
 
@@ -23,215 +19,6 @@ pub fn spawn(future: impl Future<Output = ()> + Send + 'static) -> TaskHandle {
 
 pub fn complated() -> bool {
     GLOBAL.get_or_init(Executor::new).is_empty()
-}
-
-const NORMAL: usize = 0;
-const SCHEDULED: usize = 1 << 0;
-const RUNNING: usize = 1 << 1;
-const COMPLETED: usize = 1 << 2;
-const SLEEPING: usize = 1 << 3;
-
-pub struct Task {
-    state: AtomicUsize,
-    fut: UnsafeCell<Pin<Box<dyn Future<Output = ()> + Send>>>,
-    rt: Arc<Runtime>,
-    queue_id: AtomicUsize,
-    /// can waiting for the task
-    waker: AtomicWaker,
-}
-
-pub fn task_and_handle(
-    fut: impl Future<Output = ()> + Send + 'static,
-    rt: &Arc<Runtime>,
-) -> (Arc<Task>, TaskHandle) {
-    let task = Task::new(fut, rt);
-    let handle = task.handle();
-    (task, handle)
-}
-
-impl Task {
-    /// Must call `TaskHandle::run` after calling this.
-    pub fn new(fut: impl Future<Output = ()> + Send + 'static, rt: &Arc<Runtime>) -> Arc<Self> {
-        rt.tasks.fetch_add(1, AcqRel);
-        Arc::new(Self {
-            state: AtomicUsize::new(SLEEPING),
-            fut: UnsafeCell::new(Box::pin(fut)),
-            rt: Arc::clone(rt),
-            queue_id: AtomicUsize::new(rt.local_queues.len()),
-            waker: AtomicWaker::new(),
-        })
-    }
-
-    pub fn handle(self: &Arc<Self>) -> TaskHandle {
-        TaskHandle::new(self)
-    }
-
-    pub fn schedule(self: &Arc<Self>) {
-        unsafe { TaskHandle::schedule(self.as_ref()) }
-    }
-}
-
-impl fmt::Debug for Task {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Task")
-            .field("ptr", &format!("{:p}", self as *const _))
-            .field(
-                "state",
-                &match self.state.load(Acquire) {
-                    NORMAL => "NORMAL",
-                    SCHEDULED => "SCHEDULED",
-                    RUNNING => "RUNNING",
-                    COMPLETED => "COMPLETED",
-                    SLEEPING => "SLEEPING",
-                    _ => "UNKNOWN",
-                },
-            )
-            .field("queue_id", &self.queue_id)
-            .finish()
-    }
-}
-
-#[repr(transparent)]
-pub struct TaskHandle {
-    task: Arc<Task>,
-}
-
-unsafe impl Send for TaskHandle {}
-unsafe impl Sync for TaskHandle {}
-
-impl TaskHandle {
-    pub fn new(task: &Arc<Task>) -> Self {
-        Self {
-            task: Arc::clone(task),
-        }
-    }
-
-    pub fn run(self, queue_id: usize) {
-        let task = self.task;
-
-        task.state.store(RUNNING, Release);
-
-        task.queue_id.store(queue_id, Release);
-
-        let waker = ManuallyDrop::new(scheduler(&task));
-
-        let mut cx = Context::from_waker(&waker);
-
-        match unsafe { &mut *task.fut.get() }.as_mut().poll(&mut cx) {
-            Poll::Ready(()) => {
-                task.state.store(COMPLETED, Release);
-                task.rt.tasks.fetch_sub(1, AcqRel);
-                task.waker.wake();
-            }
-            Poll::Pending => {
-                match task
-                    .state
-                    .compare_exchange(RUNNING, SLEEPING, AcqRel, Acquire)
-                {
-                    Ok(_) => {}
-                    Err(_) => {}
-                }
-            }
-        }
-    }
-
-    pub fn task(&self) -> &Arc<Task> {
-        &self.task
-    }
-
-    pub unsafe fn schedule(this: *const Task) {
-        let task = ManuallyDrop::new(Arc::from_raw(this));
-
-        // Try to schedule the task from SLEEPING.
-        match task
-            .state
-            .compare_exchange(SLEEPING, SCHEDULED, AcqRel, Acquire)
-        {
-            Ok(_) => {}
-            Err(RUNNING) => {
-                // Task has already been scheduled.
-                if task
-                    .state
-                    .compare_exchange(RUNNING, SCHEDULED, AcqRel, Acquire)
-                    .is_err()
-                {
-                    return;
-                }
-            }
-            Err(_) => {
-                return;
-            }
-        }
-
-        let queue = task.queue_id.load(Acquire);
-        let handle = Self::new(&task);
-
-        if queue != task.rt.local_queues.len() {
-            if let Err(err) = task.rt.local_queues[queue].push(handle) {
-                let handle = err.into_inner();
-                debug_assert!(task.rt.global_queue.push(handle).is_ok());
-            }
-        } else {
-            debug_assert!(task.rt.global_queue.push(handle).is_ok());
-        }
-    }
-}
-
-impl Future for TaskHandle {
-    type Output = ();
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        if self.task.state.load(Acquire) == COMPLETED {
-            Poll::Ready(())
-        } else {
-            self.task.waker.register(cx.waker());
-            Poll::Pending
-        }
-    }
-}
-
-impl Clone for TaskHandle {
-    fn clone(&self) -> Self {
-        Self {
-            task: Arc::clone(&self.task),
-        }
-    }
-}
-
-impl From<Arc<Task>> for TaskHandle {
-    fn from(task: Arc<Task>) -> Self {
-        Self { task }
-    }
-}
-
-const VTABLE: &RawWakerVTable = &{
-    #[inline]
-    unsafe fn raw_clone(ptr: *const ()) -> RawWaker {
-        Arc::increment_strong_count(ptr as *const Task);
-        RawWaker::new(ptr, VTABLE)
-    }
-
-    #[inline]
-    unsafe fn raw_wake(ptr: *const ()) {
-        raw_wake_by_ref(ptr);
-        raw_drop(ptr);
-    }
-
-    #[inline]
-    unsafe fn raw_wake_by_ref(ptr: *const ()) {
-        TaskHandle::schedule(ptr as *const Task);
-    }
-
-    #[inline]
-    unsafe fn raw_drop(ptr: *const ()) {
-        Arc::decrement_strong_count(ptr as *const Task);
-    }
-
-    RawWakerVTable::new(raw_clone, raw_wake, raw_wake_by_ref, raw_drop)
-};
-
-pub fn scheduler(task: &Arc<Task>) -> Waker {
-    let task = Arc::into_raw(task.clone());
-    unsafe { Waker::from_raw(RawWaker::new(task as *const (), VTABLE)) }
 }
 
 pub struct Executor {
@@ -289,13 +76,13 @@ impl Drop for Executor {
 
 pub struct Runtime {
     /// Global queue, all task will be sceduled in it.
-    global_queue: ConcurrentQueue<TaskHandle>,
+    pub(crate) global_queue: ConcurrentQueue<TaskHandle>,
     /// All worker's queues.
-    local_queues: Vec<Arc<ConcurrentQueue<TaskHandle>>>,
+    pub(crate) local_queues: Vec<Arc<ConcurrentQueue<TaskHandle>>>,
     /// Keep at most one worker in a loop search with None as the answer.
-    searching: Mutex<()>,
+    pub(crate) searching: Mutex<()>,
     /// Counter for task number.
-    tasks: AtomicUsize,
+    pub(crate) tasks: AtomicUsize,
 }
 
 const LOCAL_QUEUE_SIZE: usize = 512;
--- src/task/task.rs
+++ src/task/task.rs
@@ -1,57 +1,192 @@
-/// tring
-use std::future::{poll_fn, Future};
-use std::pin::{pin, Pin};
-use std::process::Output;
-use std::ptr::NonNull;
+use std::cell::UnsafeCell;
+use std::fmt;
+use std::future::Future;
+use std::mem::ManuallyDrop;
+use std::pin::Pin;
 use std::sync::atomic::{AtomicUsize, Ordering::*};
-use std::sync::OnceLock;
-use std::sync::{Arc, Mutex, RwLock};
-use std::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
-use std::thread;
-use std::time::{Duration, Instant};
+use std::sync::Arc;
+use std::task::{Context, Poll};
 
-use concurrent_queue::ConcurrentQueue;
-use num_cpus;
+use crate::executor::no_output_executor::Runtime;
+use crate::waker::AtomicWaker;
 
-use async_task::{Builder as TaskBuilder, Runnable, Schedule, Task as _};
+mod waker;
+use waker::scheduler;
 
-mod refer {
-    use async_executor;
-    use async_task::{Runnable, Task as _};
-    use smol;
-    use smol::Timer;
-    use tokio::runtime::Builder;
-    use tokio::runtime::Handle;
+pub fn task_and_handle(
+    fut: impl Future<Output = ()> + Send + 'static,
+    rt: &Arc<Runtime>,
+) -> (Arc<Task>, TaskHandle) {
+    let task = Task::new(fut, rt);
+    let handle = task.handle();
+    (task, handle)
 }
+
+const NORMAL: usize = 0;
+const SCHEDULED: usize = 1 << 0;
+const RUNNING: usize = 1 << 1;
+const COMPLETED: usize = 1 << 2;
+const SLEEPING: usize = 1 << 3;
 
 pub struct Task {
-    fut: Pin<Box<dyn Future<Output = ()>>>,
     state: AtomicUsize,
-    scheduler:Scheduler,
+    fut: UnsafeCell<Pin<Box<dyn Future<Output = ()> + Send>>>,
+    rt: Arc<Runtime>,
+    queue_id: AtomicUsize,
+    /// can waiting for the task
+    waker: AtomicWaker,
 }
 
-pub struct Scheduler {
-    local_queue: Option<Arc<ConcurrentQueue<Task>>>,
-    global_queue: Arc<ConcurrentQueue<Task>>,
+impl Task {
+    /// Must call `TaskHandle::run` after calling this.
+    pub fn new(fut: impl Future<Output = ()> + Send + 'static, rt: &Arc<Runtime>) -> Arc<Self> {
+        rt.tasks.fetch_add(1, AcqRel);
+        Arc::new(Self {
+            state: AtomicUsize::new(SLEEPING),
+            fut: UnsafeCell::new(Box::pin(fut)),
+            rt: Arc::clone(rt),
+            queue_id: AtomicUsize::new(rt.local_queues.len()),
+            waker: AtomicWaker::new(),
+        })
+    }
+
+    pub fn handle(self: &Arc<Self>) -> TaskHandle {
+        TaskHandle::new(self)
+    }
+
+    pub fn schedule(self: &Arc<Self>) {
+        unsafe { TaskHandle::schedule(self.as_ref()) }
+    }
 }
 
-// pub struct RawTask{
+impl fmt::Debug for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Task")
+            .field("ptr", &format!("{:p}", self as *const _))
+            .field(
+                "state",
+                &match self.state.load(Acquire) {
+                    NORMAL => "NORMAL",
+                    SCHEDULED => "SCHEDULED",
+                    RUNNING => "RUNNING",
+                    COMPLETED => "COMPLETED",
+                    SLEEPING => "SLEEPING",
+                    _ => "UNKNOWN",
+                },
+            )
+            .field("queue_id", &self.queue_id)
+            .finish()
+    }
+}
 
-// }
+#[repr(transparent)]
+pub struct TaskHandle {
+    task: Arc<Task>,
+}
 
-// pub struct Task<T> {
-//     state: AtomicUsize,
-//     future: F,
-//     waker: W,
-// }
+unsafe impl Send for TaskHandle {}
+unsafe impl Sync for TaskHandle {}
 
-// pub struct Todo {
-//     future: Pin<Box<dyn Future<Output=Any> + Send>>,
-//     output: Option<NonNull<()>>,
-// }
+impl TaskHandle {
+    pub fn new(task: &Arc<Task>) -> Self {
+        Self {
+            task: Arc::clone(task),
+        }
+    }
 
-// impl Todo{
-//     fn build<F,S>(future:F,schedule:S)->Todo{
+    pub fn run(self, queue_id: usize) {
+        let task = self.task;
 
-//     }
-// }
+        task.state.store(RUNNING, Release);
+
+        task.queue_id.store(queue_id, Release);
+
+        let waker = ManuallyDrop::new(scheduler(&task));
+
+        let mut cx = Context::from_waker(&waker);
+
+        match unsafe { &mut *task.fut.get() }.as_mut().poll(&mut cx) {
+            Poll::Ready(()) => {
+                task.state.store(COMPLETED, Release);
+                task.rt.tasks.fetch_sub(1, AcqRel);
+                task.waker.wake();
+            }
+            Poll::Pending => {
+                match task
+                    .state
+                    .compare_exchange(RUNNING, SLEEPING, AcqRel, Acquire)
+                {
+                    Ok(_) => {}
+                    Err(_) => {}
+                }
+            }
+        }
+    }
+
+    pub fn task(&self) -> &Arc<Task> {
+        &self.task
+    }
+
+    pub unsafe fn schedule(this: *const Task) {
+        let task = ManuallyDrop::new(Arc::from_raw(this));
+
+        // Try to schedule the task from SLEEPING.
+        match task
+            .state
+            .compare_exchange(SLEEPING, SCHEDULED, AcqRel, Acquire)
+        {
+            Ok(_) => {}
+            Err(RUNNING) => {
+                // Task has already been scheduled.
+                if task
+                    .state
+                    .compare_exchange(RUNNING, SCHEDULED, AcqRel, Acquire)
+                    .is_err()
+                {
+                    return;
+                }
+            }
+            Err(_) => {
+                return;
+            }
+        }
+
+        let queue = task.queue_id.load(Acquire);
+        let handle = Self::new(&task);
+
+        if queue != task.rt.local_queues.len() {
+            if let Err(err) = task.rt.local_queues[queue].push(handle) {
+                let handle = err.into_inner();
+                debug_assert!(task.rt.global_queue.push(handle).is_ok());
+            }
+        } else {
+            debug_assert!(task.rt.global_queue.push(handle).is_ok());
+        }
+    }
+}
+
+impl Future for TaskHandle {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.task.state.load(Acquire) == COMPLETED {
+            Poll::Ready(())
+        } else {
+            self.task.waker.register(cx.waker());
+            Poll::Pending
+        }
+    }
+}
+
+impl Clone for TaskHandle {
+    fn clone(&self) -> Self {
+        Self {
+            task: Arc::clone(&self.task),
+        }
+    }
+}
+
+impl From<Arc<Task>> for TaskHandle {
+    fn from(task: Arc<Task>) -> Self {
+        Self { task }
+    }
+}
