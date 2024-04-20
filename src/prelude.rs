12f735d20d613a465b78a3021cc3c618ba65d334
sothouth <55689222+sothouth@users.noreply.github.com>
2024-04-21 02:01:07 +0800

    write prelude

---
+++ src/prelude.rs
@@ -0,0 +1,9 @@
+pub use crate::block_on::block_on;
+pub use crate::block_on::block_on_naive;
+
+pub use crate::executor::spawn;
+
+pub use crate::blocking::spawn_blocking;
+
+pub use crate::task::OnceTaskHandle;
+pub use crate::task::TaskHandle;
